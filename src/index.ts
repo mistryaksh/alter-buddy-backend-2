843a3b986904a5c9408d58982fffeb60ee25e716
mistryaksh <mistryaksh1998@gmail.com>
2024-01-21 18:54:13 +0530

    added cors fixing

--- src/index.ts
+++ src/index.ts
@@ -44,10 +44,7 @@ class App {
      }
 
      private useCorsMiddleware(req: Request, res: Response, next: NextFunction) {
-          res.setHeader("Access-Control-Allow-Origin", [
-               "https://alterbuddy.com",
-               "https://alterbuddyadmin.netlify.app",
-          ]);
+          res.setHeader("Access-Control-Allow-Origin", "*");
           res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
           res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
           res.setHeader("Access-Control-Allow-Credentials", "true");
