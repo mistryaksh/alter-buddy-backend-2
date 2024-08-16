26aa4842b3aa6f027191d56dd868b7437a0e2d4e
Anil Prajapati <anil100699@gmail.com>
2024-08-16 22:31:26 +0530

    chat-feature-optimization

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -23,26 +23,37 @@ io.on('connection', (socket) => {
 
   // Listen for chat request from the first app
   socket.on('requestChat', (data) => {
-      console.log('Chat requested:', data);
+    console.log('Chat requested:', data);
 
-      // Emit chat request to the second app
-      socket.broadcast.emit('receiveChatRequest', data);
+    // Emit chat request to the second app
+    socket.broadcast.emit('receiveChatRequest', data);
   });
 
   // Listen for chat acceptance from the second app
   socket.on('acceptChat', (data, callback) => {
-      console.log('Chat accepted:', data);
+    console.log('Chat accepted:', data);
 
-      // Notify the first app that the chat has been accepted
-      socket.broadcast.emit('chatAccepted', data);
+    // Notify the first app that the chat has been accepted
+    socket.broadcast.emit('chatAccepted', data);
 
-      // Call the callback to acknowledge
-      if (callback) callback();
+    // Call the callback to acknowledge
+    if (callback) callback();
+  });
+
+  // Handle leaving the chat
+  socket.on('leaveChat', (data, callback) => {
+    console.log('Chat left:', data);
+
+    // Notify the other app that the chat was left
+    socket.broadcast.emit('chatLeft', data);
+
+    // Acknowledge the event to the client
+    if (callback) callback();
   });
 
   // When the client disconnects
   socket.on('disconnect', () => {
-      console.log('user disconnected:', socket.id);
+    console.log('user disconnected:', socket.id);
   });
 });
 
